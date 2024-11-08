c4d2b4f47ee4c457c65c6b9d171c6d0e90ec184d
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-08 21:43:08 +0500

    Hostel Application

--- frontend/src/components/AddApplicant.js
+++ frontend/src/components/AddApplicant.js
@@ -1,6 +1,24 @@
+
 const AddApplicant = () => {
-    return ( <div>
-        hello1
+    
+    
+    function handleChange(){
+
+    
+    }
+    return ( 
+    <div className="col-md-6 position-absolute start-50 translate-middle-x mt-5">
+    <form>
+    <label className="mb-2">
+     Student Name
+    </label>
+    <input type="text" className="form-control mb-3" name="studentName" onChange={(e)=>handleChange(e)}/>
+    <label className="mb-2">
+     Registration Number
+    </label>
+    <input type="text" className="form-control mb-3" name="studentName" onChange={(e)=>handleChange(e)}/>
+    <button className="btn btn-primary form-control">Apply</button>
+    </form>    
     </div> );
 }
  
