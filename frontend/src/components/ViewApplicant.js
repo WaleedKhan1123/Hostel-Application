ea0f24dcbe44b8c97fca5d890fa6a24abea7e8b1
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-08 21:47:22 +0500

    Hostel Application

--- frontend/src/components/ViewApplicant.js
+++ frontend/src/components/ViewApplicant.js
@@ -1,33 +1,18 @@
 const ViewApplicant= () => {
     return (
-    <div>
+    <div className="col-md-6 position-absolute start-50 translate-middle-x mt-5">
    <table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Student Name</th>
+      <th scope="col">Registration Number</th>
+      
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
       <td>Otto</td>
       <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
     </tr>
   </tbody>
 </table>
