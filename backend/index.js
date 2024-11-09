c789082ec298b7dd3265daea6836b5f5b6c8f526
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-10 00:28:33 +0500

    Hostel Application

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,6 @@
-const url = "mongodb+srv://waleedkn270:waleedkh11@hostelapplicants.fjqdy.mongodb.net/"
+import bodyParser from "body-parser";
+import cors from "corse";
+import mongoose from "mongoose";
+import express from "express";
+const app = express();
+const url = "mongodb+srv://waleedkn270:waleedkh11@hostelapplicants.fjqdy.mongodb.net/?retryWrites=true&w=majority&appName=HostelApplicants"
